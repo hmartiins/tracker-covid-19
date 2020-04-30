8e2d53404ef6ec6e76c666281c2693a321c1cda2
Henrique Martins <hmartins224@gmail.com>
2020-04-29 22:34:24 -0300

    Adding and styling the 3 cards

--- src/components/Cards/cards.js
+++ src/components/Cards/cards.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
 
-import classes from './cards.modules.css';
+import classes from './cards.module.css';
 
-const Cards = ({ data: { confirmed, recoverd, deaths, lastUpdate }}) => {
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
    
-   console.log({ confirmed, recoverd, deaths, lastUpdate });
+   console.log({ confirmed, recovered, deaths, lastUpdate });
 
    if(!confirmed){
       return 'Carregando...';
@@ -16,7 +16,40 @@ const Cards = ({ data: { confirmed, recoverd, deaths, lastUpdate }}) => {
    return(
       <div className={classes.container}>
          <Grid container justify="center" spacing={3}>
-            <Grid item component={Card}>
+         <Grid 
+               item 
+               component={Card} 
+               xs={12}
+               md={3}
+               className={[classes.card, classes.recovered].join(' ')}
+            >
+               <CardContent>
+                  <Typography color="textSecondary" gutterBottom >
+                     Recuperados
+                  </Typography>
+                  <Typography variant="h5">
+                     <CountUp 
+                        start={0}
+                        end={recovered.value}
+                        duration={2.77}
+                        separator=','
+                     />
+                  </Typography>
+                  <Typography color="textSecondary">
+                     {new Date(lastUpdate).toDateString()}
+                  </Typography>
+                  <Typography variant="body2">
+                     Número de casos recuperados
+                  </Typography>
+               </CardContent>
+            </Grid>
+            <Grid 
+               item 
+               component={Card} 
+               xs={12}
+               md={4}
+               className={[classes.card, classes.infected].join(' ')}
+            >
                <CardContent>
                   <Typography color="textSecondary" gutterBottom >
                      Infectados
@@ -36,33 +69,28 @@ const Cards = ({ data: { confirmed, recoverd, deaths, lastUpdate }}) => {
                      Número de casos ativos
                   </Typography>
                </CardContent>
-            </Grid>
-            <Grid item component={Card}>
-               <CardContent>
-                  <Typography color="textSecondary" gutterBottom >
-                     Recuperados
-                  </Typography>
-                  <Typography variant="h5">
-                     REAL DATA
-                  </Typography>
-                  <Typography color="textSecondary" gutterBottom>
-                     REAL DATE
-                  </Typography>
-                  <Typography variant="body2">
-                     Número de casos recuperados
-                  </Typography>
-               </CardContent>
-            </Grid>
-            <Grid item component={Card}>
+            </Grid> 
+            <Grid 
+               item 
+               component={Card} 
+               xs={12}
+               md={3}
+               className={[classes.card, classes.deaths].join(' ')}
+            >
                <CardContent>
                   <Typography color="textSecondary" gutterBottom >
                      Mortes
                   </Typography>
                   <Typography variant="h5">
-                     REAL DATA
+                     <CountUp 
+                        start={0}
+                        end={deaths.value}
+                        duration={2.77}
+                        separator=','
+                     />
                   </Typography>
-                  <Typography color="textSecondary" gutterBottom>
-                     REAL DATE
+                  <Typography color="textSecondary">
+                     {new Date(lastUpdate).toDateString()}
                   </Typography>
                   <Typography variant="body2">
                      Número de mortes
